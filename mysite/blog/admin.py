a4efef0aac01f32e758d95d9e241b50c03673390
Pedro Cardoso <pmtcardoso@gmail.com>
2019-03-09 14:13:18 +0000

    Add comment system

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'email', 'body')
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -12,4 +18,5 @@ class PostAdmin(admin.ModelAdmin):
     ordering = ['status', 'publish']
 
 
+admin.site.register(Comment, CommentAdmin)
 admin.site.register(Post, PostAdmin)
