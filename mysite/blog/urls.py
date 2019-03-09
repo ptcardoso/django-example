159d25083ea1d73d30192e4e2fb6695c5f2b3d05
Pedro Cardoso <pmtcardoso@gmail.com>
2019-03-09 13:56:12 +0000

    Add share by email

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 from .import views
 
-app_name='blog'
+app_name = 'blog'
 urlpatterns = [
     # post views
     url(r'^$', views.PostListView.as_view(), name='post_list'),
-    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<post>[-\w]+)/$', views.post_detail, name='post_detail')
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<post>[-\w]+)/$', views.post_detail, name='post_detail'),
+    url(r'^(?P<post_id>\d+)/share/$', views.post_share, name='post_share')
 ]
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -119,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'your_account@gmail.com'
+EMAIL_HOST_PASSWORD = 'your_password'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
